c8e4a94d56313e1a71eb66fd131aaf11e8d77f73
Dheeraj <anubhavgupta1012@gmail.com>
2022-08-26 23:16:10 +0530

    Code Refactor for Spring.xml

--- src/main/java/com/shapes/Drawing.java
+++ src/main/java/com/shapes/Drawing.java
@@ -8,10 +8,10 @@ public class Drawing {
     public static void main(String[] args) {
 
         ApplicationContext ac = new ClassPathXmlApplicationContext("Spring.xml");
-        Triangle t = (Triangle) ac.getBean("tri");
+        Triangle triangle = (Triangle) ac.getBean("tri");
         Triangle t2 = (Triangle) ac.getBean("tri2");
         Triangle t3 = (Triangle) ac.getBean("tri3");
-        System.out.println(t + "\n" + t2 + "\n" + t3);
-        t.draw();
+        System.out.println(triangle + "\n" + t2 + "\n" + t3);
+        triangle.draw();
     }
 }
--- src/main/java/com/shapes/collection/Drawing.java
+++ src/main/java/com/shapes/collection/Drawing.java
@@ -5,13 +5,9 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Drawing {
-
-	public static void main(String[] args) {
-
-		ApplicationContext ac=new ClassPathXmlApplicationContext("Config.xml");
-		Triangle t=(Triangle)ac.getBean("ABC");
-		t.draw();
-	
-	}
-
+    public static void main(String[] args) {
+        ApplicationContext ac = new ClassPathXmlApplicationContext("Config.xml");
+        Triangle t = (Triangle) ac.getBean("ABC");
+        t.draw();
+    }
 }
