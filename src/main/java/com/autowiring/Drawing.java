2b73533f6d4c118ba06332b8c3b22f5b010d4be1
Dheeraj <anubhavgupta1012@gmail.com>
2022-08-27 14:28:12 +0530

    Autowiring refactored

--- src/main/java/com/autowiring/Drawing.java
+++ src/main/java/com/autowiring/Drawing.java
@@ -1,20 +1,15 @@
- package com.autowiring;
+package com.autowiring;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Drawing {
-
-	public static void main(String[] args) {
-
-		BeanFactory ac=new XmlBeanFactory(new FileSystemResource("autowiring.xml"));
-		Triangle t=(Triangle)ac.getBean("abc");
-		Triangle t2=(Triangle)ac.getBean("abc");
-		Triangle t3=(Triangle)ac.getBean("abc");
-		System.out.println(t+"\n"+t2+"\n"+t3);
-		t.draw();
-	
-	}
-
+    public static void main(String[] args) {
+        ApplicationContext ac = new ClassPathXmlApplicationContext("autowiring.xml");
+        Triangle t = (Triangle) ac.getBean("abc");
+        Triangle t2 = (Triangle) ac.getBean("abc");
+        Triangle t3 = (Triangle) ac.getBean("abc");
+        System.out.println(t + "\n" + t2 + "\n" + t3);
+        t.draw();
+    }
 }
